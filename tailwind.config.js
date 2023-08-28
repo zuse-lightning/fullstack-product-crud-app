1138e71b45ed7d1ee5bad0c6261424ad964b66f2
James Geib <james@zuse.com>
2023-08-28 14:51:52 -0400

    added to package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     container:{
       center: true,
@@ -10,4 +12,3 @@ module.exports = {
   },
   plugins: [],
 }
-
